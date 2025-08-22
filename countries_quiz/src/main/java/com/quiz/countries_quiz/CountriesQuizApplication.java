2b36f85d611f8c0a77d00f0378dc53daee4675b8
Hemanth0701 <hemanthkumar0701m@gmail.com>
2025-08-22 19:08:55 +0530

    Initial commit for quiz webapp

---
+++ countries_quiz/src/main/java/com/quiz/countries_quiz/CountriesQuizApplication.java
@@ -0,0 +1,13 @@
+package com.quiz.countries_quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountriesQuizApplication.class, args);
+	}
+
+}
